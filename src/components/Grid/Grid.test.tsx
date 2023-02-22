736946e45c906c7d9ea11e4dd325a0ce9adfa4fa
Rob Kogan <rkogan@gmail.com>
2023-02-21 21:03:09 -0800

    removes unused import

--- src/components/Grid/Grid.test.tsx
+++ src/components/Grid/Grid.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 import Grid from "./Grid";
 
 test("renders grid", () => {
